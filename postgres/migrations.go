9fba8115c455cd2a7a0dee89cc7c12f9db36b83c
Artem Kornilaev <artemkornilaev1@gmail.com>
2025-05-10 22:37:35 +0400

    add 'RunMigrations' func

---
+++ postgres/migrations.go
@@ -0,0 +1,36 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+)
+
+func (p *Postgres) RunMigrations(ctx context.Context, migrationPath string) error {
+	connStr := p.Pool.Config().ConnString()
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return fmt.Errorf("failed to open db connection: %w", err)
+	}
+	defer db.Close()
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create postgres driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://"+migrationPath, "postgres", driver)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to apply migrations: %w", err)
+	}
+
+	return nil
+}
