49017eaee44a7a760e49c2e93053fd6f93818992
Artem Kornilaev <artemkornilaev1@gmail.com>
2025-05-20 15:10:14 +0400

    update migrations

--- postgres/migrations.go
+++ postgres/migrations.go
@@ -6,10 +6,16 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
+	pgxMigrateDriver "github.com/golang-migrate/migrate/v4/database/pgx/v5"
+	_ "github.com/golang-migrate/migrate/v4/source/file" // Для "file://"
+	// Нужен для приведения типа p.Pool
 )
 
 func (p *Postgres) RunMigrations(ctx context.Context, migrationPath string) error {
+	if p.Pool == nil {
+		return fmt.Errorf("migrations: pgxpool.Pool is nil in Postgres struct")
+	}
+
 	connStr := p.Pool.Config().ConnString()
 
 	db, err := sql.Open("postgres", connStr)
@@ -18,7 +24,7 @@ func (p *Postgres) RunMigrations(ctx context.Context, migrationPath string) erro
 	}
 	defer db.Close()
 
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	driver, err := pgxMigrateDriver.WithInstance(db, &pgxMigrateDriver.Config{})
 	if err != nil {
 		return fmt.Errorf("failed to create postgres driver: %w", err)
 	}
@@ -31,6 +37,8 @@ func (p *Postgres) RunMigrations(ctx context.Context, migrationPath string) erro
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		return fmt.Errorf("failed to apply migrations: %w", err)
 	}
+
+	m.Close()
 
 	return nil
 }
